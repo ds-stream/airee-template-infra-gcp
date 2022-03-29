e9d058f21742f956586101a9b135f01ebaab82a7
Bakiera <kar.bakiera@gmail.com>
2022-03-29 14:06:15 +0200

    Adding the destroy_infra script.

---
+++ {{cookiecutter.repo_name}}/destroy_infra.sh
@@ -0,0 +1,5 @@
+terraform state rm kubernetes_config_map.airflow_cluster
+terraform state rm kubernetes_namespace.airflow_cluster
+terraform state rm google_sql_user.users
+terraform state rm google_sql_database.database
+terraform destroy
