369a524ae4c76227f161dbf18531713f975af59c
Piotr Lewinski <piotr.lewinski@dsstream.com>
2022-04-10 20:33:36 +0200

    CI/CD template

---
+++ {{cookiecutter.repo_name}}/sync_cloud.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#import json variables
+for env in $(cat status.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do export $env; done
+
+#auth gcp
+gcloud auth activate-service-account ${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT} --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud config set project ${GCP_PROJECT}
+
+#sync infr
+if [ "${status}" == "up" ]; then
+	cd service && terraform init && terraform apply --auto-approve
+elif [ "${status}" == "down" ]; then
+	cd service && terraform init && sh ../destroy_infra.sh
+else 
+     echo "Json status does not allow to apply changes"
+fi
