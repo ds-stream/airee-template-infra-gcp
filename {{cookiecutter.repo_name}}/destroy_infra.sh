52e543af2fac1e7d13a6b190d66bfe7c7890b35a
lewinskip <100766657+lewinskip@users.noreply.github.com>
2022-04-14 02:23:30 +0200

    Update destroy_infra.sh

--- {{cookiecutter.repo_name}}/destroy_infra.sh
+++ {{cookiecutter.repo_name}}/destroy_infra.sh
@@ -1,5 +1,6 @@
 terraform state rm kubernetes_config_map.airflow_cluster
 terraform state rm kubernetes_namespace.airflow_cluster
+terraform state rm kubernetes_namespace.flux_system
 terraform state rm google_sql_user.users
 terraform state rm google_sql_database.database
 terraform destroy --auto-approve
