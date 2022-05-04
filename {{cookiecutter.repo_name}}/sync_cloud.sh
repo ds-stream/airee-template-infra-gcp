335b044c6b82fa5ca8bf664a82f753317d8a848d
Piotr Lewinski <piotr.lewinski@dsstream.com>
2022-05-04 19:58:56 +0200

    divide pipelines

--- {{cookiecutter.repo_name}}/sync_cloud.sh
+++ {{cookiecutter.repo_name}}/sync_cloud.sh
@@ -4,13 +4,13 @@ for env in $(cat status.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring
 
 #auth gcp
 gcloud auth activate-service-account ${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT} --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-gcloud config set project ${GCP_PROJECT}
+gcloud config set project {{cookiecutter.project_id}}
 
 #sync infr
 if [ "${status}" == "up" ]; then
 	cd service && terraform init && terraform apply --auto-approve
 elif [ "${status}" == "down" ]; then
-        gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${GCP_PROJECT} && \
+	gcloud container clusters get-credentials {{cookiecutter.cluster_name}}-{{cookiecutter.workspace}} --region {{cookiecutter.region}} --project {{cookiecutter.project_id}} && \
 	cd service && terraform init && sh ../destroy_infra.sh
 else 
      echo "Json status does not allow to apply changes"
