efe317e1377194f7b87ab04d9bf652b78f782418
michalmi-dss <michal.milik@dsstream.com>
2022-06-21 09:22:47 +0200

    hardcoded project_id

--- {{cookiecutter.repo_name}}/{{cookiecutter.storage_folder_name}}/variables.tf
+++ {{cookiecutter.repo_name}}/{{cookiecutter.storage_folder_name}}/variables.tf
@@ -1,6 +1,6 @@
 # Can we somehow make global variables? Not supported by Hashicorp
 variable "project_id" {
-  default = "dsstream-airflowk8s"
+  default = "{{cookiecutter.project_id}}"
 }
 variable "region" {
   default = "asia-northeast1"
