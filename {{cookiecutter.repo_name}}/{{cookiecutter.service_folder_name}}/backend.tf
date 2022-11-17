d534075670e36444e9fb3a1f7a06d2fdec8bde6b
Jakub Olszewski <jakub.olszewski@dsstream.com>
2022-11-17 13:57:01 +0100

    fix: add specify env in workspace name

--- {{cookiecutter.repo_name}}/{{cookiecutter.service_folder_name}}/backend.tf
+++ {{cookiecutter.repo_name}}/{{cookiecutter.service_folder_name}}/backend.tf
@@ -2,6 +2,6 @@
 terraform {
   backend "gcs" {
     bucket = "{{cookiecutter.tf_backend}}"
-    prefix = "terraform-{{cookiecutter.workspace}}/state/{{cookiecutter.cert_name}}/"
+    prefix = "terraform-{{cookiecutter.workspace}}/state/{{cookiecutter.env}}/"
   }
 }
